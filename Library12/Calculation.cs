ed216b588581d32e0a18f1026746fa5761f4c407
KAT KAT <kass.kiss@yandex.ru>
2021-10-22 01:51:26 +0300

    Добавьте файлы проекта.

---
+++ Library12/Calculation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoubleDimensionArrayManager;
+
+namespace Library12
+{
+    public static class Calculation
+    {
+        public static string MinimalNumber(this MyArray myarray)
+        {
+            List<int> biggestColumnValues = new List<int> { };
+
+            for (int i = 0; i < myarray.ColumnLength; i++)
+            {
+                int maxValue = 1;
+
+                for (int j = 0; j < myarray.RowLength; j++)
+                {
+                    if (myarray[j, i] > maxValue)
+                    {
+                        maxValue = myarray[j, i];
+                    }
+
+                    if (j == myarray.RowLength - 1)
+                    {
+                        biggestColumnValues.Add(maxValue);
+                    }
+                }
+            }
+            return biggestColumnValues.Min().ToString();
+        }
+    }
+}
